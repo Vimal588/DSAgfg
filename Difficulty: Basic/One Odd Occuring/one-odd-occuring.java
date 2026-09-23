a352d885a160ce94339a3cd468dc559ef9aa50f6
Vimal Kumar Yadav <yadavvimalkumar591@gmail.com>
2026-09-23 16:33:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/One Odd Occuring/one-odd-occuring.java
@@ -0,0 +1,13 @@
+class Solution {
+    // Method to find the element with odd occurrence in given array
+    int getOddOccurrence(int[] arr) {
+        // code here
+        int ans = 0;
+
+              for (int i = 0; i < arr.length; i++) {
+                  ans = ans ^ arr[i];
+              }
+
+              return ans;
+    }
+}
